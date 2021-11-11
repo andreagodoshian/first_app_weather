c1adba779334774b15dda60a320feac002bbcbf7
Андрия <94014358+godoshian@users.noreply.github.com>
2021-11-11 11:42:10 -0500

    firstApp - weather
    i'm so nervous 😨

---
+++ script.js
@@ -0,0 +1,83 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayWeatherConditions(response) {
+  console.log(response.data);
+  document.querySelector("h4").innerHTML = response.data.name;
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#current-condition").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#current-humid").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#current-low").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+  document.querySelector("#current-high").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#sunrise").innerHTML = response.data.sys.sunrise;
+  document.querySelector("#sunset").innerHTML = response.data.sys.sunset;
+}
+
+// START HERE - since this is all search based, call Open Weather first
+function callOpenWeather(city) {
+  // DON'T FORGET TO INSTALL AXIOS!!!!!!!
+  let openWeatherKey = "54061155f51c8e7d4989e763fe60a16c";
+  let openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWeatherKey}&units=metric`;
+  axios.get(openWeatherUrl).then(displayWeatherConditions);
+}
+
+function chooseCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  callOpenWeather(city);
+}
+
+function searchLocation(position) {
+  let openWeatherKey = "54061155f51c8e7d4989e763fe60a16c";
+  let openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${openWeatherKey}&units=metric`;
+
+  axios.get(openWeatherUrl).then(displayWeatherConditions);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let dateElement = document.querySelector("#current-date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#city-searchbar");
+searchForm.addEventListener("submit", chooseCity);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+callOpenWeather("Pontiac");
